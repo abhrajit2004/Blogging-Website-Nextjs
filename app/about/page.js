15a2de0aad56a3a03a083779de03ddc460fdb151
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-05-10 17:30:59 +0530

    third commit

--- app/about/page.js
+++ app/about/page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Image from 'next/image';
 
 
 export const metadata = {
@@ -14,8 +15,8 @@ const About = () => {
         <p className='about'>I, Abhrajit Gupta, a 2nd year engineering student from IT department, am going to share my frontend development experience with you all through blogposts. I am also interested in Machine Learning and currently making a project on it. So, to get all my blog updates you can subscribe to my website. Thanks for reading :)</p>
         <h2 className="techstacks text-3xl font-bold">Tech Stacks on I have worked so far</h2>
         <div className="stacks flex flex-wrap justify-center items-center gap-6">
-          <img src="react.png" width={200} height={200} alt="" />
-          <img className='invert' src="next.svg" width={200} height={200} alt="" />
+          <Image src="/react.png" width={200} height={200} alt="" />
+          <Image className='invert' src="/next.svg" width={200} height={200} alt="" />
         </div>
         <button className='bg-gray-900 px-4 py-2 rounded-lg transition-all hover:bg-gray-800 border border-white' type="submit">Subscribe to my website</button>
         
