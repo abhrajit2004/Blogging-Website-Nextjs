c4dce95b8b6795d660e92939506531eb444c1faf
abhrajit2004 <abhrajitgupta.it2022@nsec.ac.in>
2024-05-11 18:35:05 +0530

    improved the backend

--- db/connectDb.js
+++ db/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try{
-        const conn = await mongoose.connect(`mongodb://localhost:27017/contactData`, {
+        const conn = await mongoose.connect(`${process.env.URI}`, {
             useNewUrlParser: true,
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);      
